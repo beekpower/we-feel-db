65d0f0d78e571f4305e37678528e1fc22fffe001
nicholas <nicholas@pop-os.localdomain>
2020-09-03 00:06:44 -0400

    Create docker compose and hourly sql

---
+++ hourly.sql
@@ -0,0 +1,11 @@
+CREATE TABLE hourly (
+  timestamp timestamp,
+  total integer,
+  anger integer,
+  fear integer,
+  joy integer,
+  love integer,
+  other integer,
+  sadness integer,
+  surprise integer
+);
